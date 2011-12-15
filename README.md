53123799fe1f8b937c7660926c04955f8280c1e6
Brandon Philips <brandon@ifup.org>
2011-12-14 22:22:57 -0800

    Initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# Buildbot on NODE
+
+Simple library for polling buildbot builds.
+
+# Credits
+
+Library pulled out from Tomaž Muraus's node-buildbot-github.
+https://github.com/Kami/node-buildbot-github
---
+++ tests/buildbot.js
@@ -0,0 +1,32 @@
+var pollers = require('../lib/pollers');
+var builds = require('./fixtures/builds');
+var http = require('http');
+var async = require('async');
+var logmagic = require('logmagic');
+
+exports['test_buildbot_in_progress'] = function(test, assert) {
+  http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/json'});
+    res.end(JSON.stringify(builds.in_progress_build) + "\n");
+    req.connection.destroy()
+  }).listen(8000, "127.0.0.1");
+
+  poller = new pollers.BuildbotPoller({
+    "host": "127.0.0.1",
+    "port": "8000",
+    "secure": false,
+    "username": null,
+    "password": null,
+    "change_hook_path": "/change_hook/base",
+    "category": "pull-requests",
+    "builder_name": "Linux",
+    "num_builds": 1
+  }, 0);
+
+  poller.start();
+  poller.on('in_progress_build', function(build) {
+    poller.stop();
+    assert.equal(Object.keys(builds.in_progress_build)[0], build.number.toString());
+  });
+  test.finish();
+}
