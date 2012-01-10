07e46fc515192bd1e545256f90f9d991c5b93224
Brandon Philips <brandon.philips@rackspace.com>
2012-01-09 16:49:36 -0800

    lib: add functions from dreadnot
    this commit adds the functions from dreadnot along with a test of
    getRevision.
    
    TODO: add test for ensureRevisionBuilt

--- tests/builder.js
+++ tests/builder.js
@@ -3,11 +3,12 @@ var nock = require('nock');
 var request = require('request');
 var async = require('async');
 var logmagic = require('logmagic');
+var fs = require('fs');
 var sprintf = require('sprintf').sprintf;
 
 var base_url = "http://buildbot.example.com"
 
-function setupPollTest() {
+function setupTest() {
   var bb = new Buildbot(base_url);
   var builder = bb.builder('Linux', {'poll_interval': 0});
   return builder;
@@ -17,7 +18,7 @@ exports['test_buildbot_build_in_progress'] = function(test, assert) {
   var scope = nock(base_url)
                 .get('/json/builders/Linux/builds/')
                 .replyWithFile(200, __dirname + '/fixtures/in_progress.json');
-  bb = setupPollTest();
+  bb = setupTest();
   bb.start();
   bb.on('in_progress_build', function(build) {
     bb.stop();
@@ -31,7 +32,7 @@ exports['test_buildbot_build_finished'] = function(test, assert) {
   var scope = nock(base_url)
                 .get('/json/builders/Linux/builds/')
                 .replyWithFile(200, __dirname + '/fixtures/completed_build.json');
-  bb = setupPollTest();
+  bb = setupTest();
   bb.start();
   bb.on('new_build', function(build) {
     bb.stop();
@@ -42,7 +43,7 @@ exports['test_buildbot_build_finished'] = function(test, assert) {
 }
 
 exports['test_buildbot_builder_build'] = function(test, assert) {
-  bb = setupPollTest();
+  bb = setupTest();
   var revision = 'deadbeef';
 
   var body = 'username=philips&revision=deadbeef';
@@ -51,6 +52,21 @@ exports['test_buildbot_builder_build'] = function(test, assert) {
                   .post(path, body)
                   .reply(201, 'OK');
   bb.build(revision, {name: 'philips'}, function(error, body) {
+    scope.done();
+    test.finish();
+  });
+}
+
+exports['test_buildbot_builder_get_revision'] = function(test, assert) {
+  var filename = __dirname + '/fixtures/completed_build.json';
+
+  var builds = JSON.parse(fs.readFileSync(filename));
+  var scope = nock(base_url)
+                .get('/json/builders/Linux/builds/')
+                .replyWithFile(200, filename);
+  bb = setupTest();
+  bb.getRevision('115044', function(error, build) {
+    assert.equal(build['number'], builds['-1']['number']);
     scope.done();
     test.finish();
   });
